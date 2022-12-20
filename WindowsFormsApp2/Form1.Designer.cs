e1a70bcfb6463abe3fb04435c308cda2aec2d4e8
YHT97 <91371115+YHT97@users.noreply.github.com>
2022-12-20 22:33:39 +0300

    Initial commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(411, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.istockphoto_482437792_170667a;
+            this.panel2.Location = new System.Drawing.Point(16, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(495, 355);
+            this.panel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(536, 439);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Panel panel2;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.Panel panel1;
+
+        #endregion
+    }
+}
---
+++ WindowsFormsApp2/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Net.Mime;
+
+namespace WindowsFormsApp2
+{
+    public class Player
+    {
+        private int x;
+        private int y;
+        private Image img;
+        private string player_name;
+
+        public Player(int x, int y, string playerName)
+        {
+            this.x = x;
+            this.y = y;
+            player_name = playerName;
+        }
+    }
+}
